35d430728461c9bb3a69438d2bf23ade13b13acb
Grigore Crudu <gcrudu@mail.ru>
2015-12-09 20:31:59 +0000

    Remove Availability from Pacient

---
+++ model/availability.js
@@ -0,0 +1,15 @@
+/**
+ * Created by home on 26.08.2015.
+ */
+
+
+
+(function() {
+    module.exports = function (args) {
+        assert.ok(args.userId, "User ID is mandatory");
+
+    }
+})();
+
+
+
