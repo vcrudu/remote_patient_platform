06146a591059c06fa57bab56eed242aaa6d2c117
Victor Lungu <victor@e-cubed.com>
2015-12-12 17:16:30 +0200

    commit

--- public/config/providerStatesConfig.js
+++ public/config/providerStatesConfig.js
@@ -20,6 +20,10 @@ angular.module('app').config(['$stateProvider', function ($stateProvider) {
         templateUrl: "provider/appointments/provider.appointments.view.html",
         controller: 'providerAppointmentsViewCtrl',
         controllerAs: 'vm'
+    }).state("provider.patient", {
+          url: "/patient/:userName",
+          templateUrl: "provider/appointments/provider.appointments.patient.html",
+          controller: 'providerAppointmentsPatientViewCtrl',
     }).state("provider.settings", {
         url: "/provider.settings",
         templateUrl: "provider/settings/provider.settings.html",
