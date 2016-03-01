aca3395808c93d13b15e437708645c918d9b8910
Victor Lungu <victor@e-cubed.info>
2016-03-01 14:22:47 +0200

    commit vital signs charts

--- public/mobile/react/www/app/vital-signs/js/patient-vital-sgns.js
+++ public/mobile/react/www/app/vital-signs/js/patient-vital-sgns.js
@@ -40,7 +40,36 @@
                 svg: undefined
             };
         },
+        fillLines: function (g, data, x, y) {
+            g.selectAll("line").remove();
+            g.selectAll("line").data(data).enter().append("line").attr("class", "circles-line").attr("x1", function (d) {
+                return x(d.dateTime);
+            }).attr("y1", function (d) {
+                return y(d.line.y1);
+            }).attr("x2", function (d) {
+                return x(d.dateTime);
+            }).attr("y2", function (d) {
+                return y(d.line.y2);
+            });
+        },
+        fillCircles: function (g, tip, data, x, y) {
+            g.selectAll('circle').remove();
+            g.selectAll("circle").data(data).enter().append("circle").attr("cx", function (d) {
+                return x(d.dateTime);
+            }).attr("cy", function (d) {
+                return y(d.value);
+            }).attr("r", 5).attr("class", 'circle').attr("data-legend", function (d) {
+                return d.label;
+            }).style("fill", function (d) {
+                return d.color;
+            }).on("click", function (d) {
+                tip.show(d);d3.select(this).style("stroke", "black").style("fill", d.color).style("stroke-width", 2);
+            }).on("mouseout", function (d) {
+                tip.hide(d);d3.select(this).style("stroke", "black").style("fill", d.color).style("stroke-width", 0);
+            });
+        },
         drawGraphic: function (props) {
+            var component = this;
             var chartRef = $(this.refs.graphic);
             var chartContextRef = $(this.refs.graphicContext);
             var graphicWrapper = $(this.refs.graphicWrapper);
@@ -55,12 +84,14 @@
             var margin = { top: 10, right: 15, bottom: 25, left: 35 };
             var width = chartRef.width() - margin.left - margin.right;
             var height = Math.ceil(width * props.aspectHeight / props.aspectWidth) - margin.top - margin.bottom;
-            var height2 = Math.ceil(width * 2.5 / props.aspectWidth) - margin.top - margin.bottom;
+            var height2 = Math.ceil(width * 2 / props.aspectWidth) - margin.top - margin.bottom;
 
             var num_ticks = props.ticks;
 
             if (chartRef.width() < props.mobileThreshold) {
                 num_ticks = props.mobileTicks;
+
+                height2 = Math.ceil(width * 3 / props.aspectWidth) - margin.top - margin.bottom;
             }
 
             // clear out existing graphics
@@ -116,40 +147,48 @@
 
             var focus = svg.append("g").attr("class", "focus").attr("transform", "translate(" + margin.left + "," + margin.top + ")");
 
-            var focus2 = svg.append("g").attr("class", "focus").attr("transform", "translate(" + margin.left + "," + margin.top + ")");
-
             var svg1 = d3.select(chartContextRef[0]).append("svg").attr("width", width + margin.left + margin.right).attr("height", height2 + margin.top + margin.bottom);
 
             var context = svg1.append("g").attr("class", "context").attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+
+            var tip = d3.tip().attr('class', 'd3-tip').offset([-10, 0]).html(function (d) {
+                return d.value + " " + props.unit;
+            });
+
+            svg.call(tip);
 
             var brush = d3.svg.brush().x(x2).on("brush", function () {
                 x.domain(brush.empty() ? x2.domain() : brush.extent());
                 focus.select(".area").attr("d", area);
                 focus.select(".line").attr("d", line);
-                focus.selectAll('circle').attr('cx', function (d) {
-                    return x(d.dateTime);
-                }).attr('cy', function (d) {
-                    return y(d.value);
-                });
+
+                if (props.type == "blood_pressure") {
+                    component.fillLines(focus, data, x, y);
+                }
+
+                component.fillCircles(focus, tip, data, x, y);
+
                 focus.select(".x.axis").call(xAxis);
 
-                // Reset zoom scale's domain
+                // Reset zoom scale', s domain
                 zoom.x(x);
             });
 
             var zoom = d3.behavior.zoom().on("zoom", function () {
-                debugger;
                 focus.select(".area").attr("d", area);
                 focus.select(".line").attr("d", line);
+
                 focus.selectAll('circle').attr('cx', function (d) {
                     return x(d.dateTime);
                 }).attr('cy', function (d) {
                     return y(d.value);
                 });
+
                 focus.select(".x.axis").call(xAxis);
 
                 // Force changing brush range
                 brush.extent(x.domain());
+
                 svg.select(".brush").call(brush);
             });
 
@@ -157,7 +196,6 @@
             if (props.type != "blood_pressure") {
                 var tempArray1 = [];
                 for (var i = 0; i < props.dataSource.values.length; i++) {
-
                     tempArray1.push({
                         dateTime: moment(props.dataSource.values[i].time),
                         value: props.dataSource.values[i].value,
@@ -169,19 +207,13 @@
             } else {
                 var tempArray = [];
                 for (var i = 0; i < props.dataSource.values.length; i++) {
-
-                    if (i == 0) {
-                        tempArray.push({
-                            dateTime: moment(props.dataSource.values[i].time).add(-1, 'days'),
-                            value: props.dataSource.values[i].value.systolic,
-                            color: "blue",
-                            label: props.dataSource.label
-                        });
-                    }
-
                     tempArray.push({
                         dateTime: moment(props.dataSource.values[i].time),
                         value: props.dataSource.values[i].value.systolic,
+                        line: {
+                            y1: props.dataSource.values[i].value.systolic,
+                            y2: props.dataSource.values[i].value.diastolic
+                        },
                         color: "blue",
                         label: props.dataSource.label
                     });
@@ -189,6 +221,10 @@
                     tempArray.push({
                         dateTime: moment(props.dataSource.values[i].time),
                         value: props.dataSource.values[i].value.diastolic,
+                        line: {
+                            y1: props.dataSource.values[i].value.systolic,
+                            y2: props.dataSource.values[i].value.diastolic
+                        },
                         color: "red",
                         label: props.dataSource.label
                     });
@@ -216,21 +252,11 @@
                 focus.append("path").datum(data).attr("class", "line").attr("width", width).attr("d", line);
             }
 
-            var tip = d3.tip().attr('class', 'd3-tip').offset([-10, 0]).html(function (d) {
-                return d.value + " " + props.unit;
-            });
+            if (props.type == "blood_pressure") {
+                component.fillLines(focus, data, x, y);
+            }
 
-            svg.call(tip);
-
-            var circles = focus.selectAll('circle').data(data).enter().append("circle").attr("cx", function (d) {
-                return x(d.dateTime);
-            }).attr("cy", function (d) {
-                return y(d.value);
-            }).attr("r", 5).attr("class", 'circle').attr("data-legend", function (d) {
-                return d.label;
-            }).style("fill", function (d) {
-                return d.color;
-            }).on("click", tip.show).on("mouseover", tip.show).on("mouseout", tip.hide);
+            component.fillCircles(focus, tip, data, x, y);
 
             focus.append("g").attr("class", "x axis").attr("transform", "translate(0," + height + ")").call(xAxis);
 
@@ -243,9 +269,9 @@
                     return x2(d.dateTime);
                 }).attr("cy", function (d) {
                     return y2(d.value);
-                }).attr("r", 3).style("fill", function (d) {
+                }).attr("r", 3).style("stroke", function (d) {
                     return d.color;
-                });
+                }).style("fill", "none").style("stroke-width", 2);
             }
 
             context.append("g").attr("class", "x axis").attr("transform", "translate(0," + height2 + ")").call(xAxis2);
