d1499a6d65553b86ffea76aec9e08305968d5c1e
corneliu <corneliu@e-cubed.info>
2016-04-01 21:44:03 +0300

    Thermometer

--- public/mobile/react/www/app/devices/js/Thermometer.js
+++ public/mobile/react/www/app/devices/js/Thermometer.js
@@ -82,13 +82,13 @@
         render: function () {
             return React.createElement(
                 "div",
-                { className: "row buttonsContainer" },
-                React.createElement("div", { className: "col-xs-8" }),
+                { className: "row has-separator buttons-container" },
+                React.createElement("div", { className: "col-xs-6" }),
                 React.createElement(
                     "div",
-                    { className: "col-xs-4" },
-                    this.state.nextButtonVisibility ? React.createElement("input", { type: "button", className: "btn btn-default", value: "Next", onClick: this.handleNext }) : null,
-                    this.state.doneButtonVisibility ? React.createElement("input", { type: "button", className: "btn btn-default", value: "Done", onClick: this.handleDone }) : null
+                    { className: "col-xs-6" },
+                    this.state.nextButtonVisibility ? React.createElement("input", { type: "button", className: "btn btn-default pull-right", value: "Next", onClick: this.handleNext }) : null,
+                    this.state.doneButtonVisibility ? React.createElement("input", { type: "button", className: "btn btn-default pull-right", value: "Done", onClick: this.handleDone }) : null
                 )
             );
         }
