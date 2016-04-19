6fee1258c43535882507ecfb365852dd0b075447
Victor Lungu <victor@e-cubed.info>
2016-04-19 14:34:05 +0300

    .

--- public/mobile/react/www/app/devices/js/BloodPressure-measure.js
+++ public/mobile/react/www/app/devices/js/BloodPressure-measure.js
@@ -16,15 +16,30 @@
                 tryAgainButtonVisibility: false,
                 cancelButtonVisibility: false,
                 doneButtonVisibility: false,
-                value: undefined
+                value: undefined,
+                progressBar: undefined
             };
         },
         componentDidMount: function () {
             var component = this;
+
+            var intObj = {
+                template: 3,
+                parent: '.progress-bar-indeterminate' // this option will insert bar HTML into this parent Element
+            };
+            var indeterminateProgress = new Mprogress(intObj);
+            component.setState({
+                progressBar: indeterminateProgress
+            });
+
+            indeterminateProgress.start();
             Bridge.DeviceReceiver.takeMeasure(component.props.deviceModelType, component.props.deviceModel, function (result) {
                 if (result.success) {
                     switch (result.data.status) {
                         case "measure-received":
+                            if (component.state.progressBar) {
+                                component.state.progressBar.end();
+                            }
                             component.setState({
                                 nextButtonVisibility: true,
                                 tryAgainButtonVisibility: false,
@@ -34,11 +49,8 @@
                             $(component.props.carouselWizard).carousel("next");
                             break;
                         case "measure-timeout":
-                            component.setState({
-                                nextButtonVisibility: false,
-                                tryAgainButtonVisibility: true,
-                                cancelButtonVisibility: true
-                            });
+                            component.handleTryAgain();
+                            break;
                     }
                 }
             });
