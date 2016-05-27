c3e2d4423283ffb2926cec0403a40fcd884eb95d
Victor Lungu <victor@e-cubed.info>
2016-05-27 11:49:10 +0300

    .

--- public/mainCtrl.js
+++ public/mainCtrl.js
@@ -5,24 +5,34 @@
 (function(){
     angular.module('app').
     controller('mainCtrl', ['$scope','$state','$localStorage','toastr','authService',function($scope, $state, $localStorage, toastr, authService){
-            $scope.extr_page="extr-page";
-            $scope.bodyClass="desktop-detected pace-done";
+        $scope.extr_page="extr-page";
+        $scope.bodyClass="desktop-detected pace-done";
 
-            $scope.containerClass="container";
-            function setLayout(){
-                if($localStorage.user){
-                    $scope.extr_page="";
-                    $scope.userName = authService.getUserName();
-                    $scope.containerClass="";
-                }
+        $scope.containerClass="container";
+
+        var hash = window.location.hash;
+        if (hash.indexOf("login") == -1) {
+            setLayout();
+            $scope.userName = authService.getUserName();
+            $scope.containerClass="";
+        }
+
+        function setLayout(){
+            if($localStorage.user){
+                $scope.extr_page="";
+                $scope.userName = authService.getUserName();
+                $scope.containerClass="";
             }
+        }
 
         $scope.$on('signin', function(){
             setLayout();
             $scope.userName = authService.getUserName();
             $scope.containerClass="";
         });
+
         $scope.$on('login', function(){
+            debugger;
             $scope.extr_page="extr-page";
         });
 
