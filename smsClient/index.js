488651d71d52177156223a701c3aa49605745143
Victor Crudu <vcrudu@hotmail.com>
2016-06-11 18:44:11 +0100

    Added sendProviderAppointmentSms

--- smsClient/index.js
+++ smsClient/index.js
@@ -5,6 +5,7 @@
 (function() {
     var clockwork = require('clockwork')({key: '14fa969186419ef51679bd6ac938834e37e05b32'});
     var userDetailsRepository = require('../repositories/usersDetailsRepository');
+    var providersRepository = require('../repositories/providersRepository');
     var moment = require('moment');
 
     function sendAppointmentSms(toUser, slotDateTimeEpoch, callback) {
@@ -13,11 +14,11 @@
             slotDateTime.setTime(slotDateTimeEpoch);
             var momentSlotDateTime = moment(slotDateTime);
             var content = "Dear " + data.title + " " + data.firstname + " " + data.surname + ", " +
-                        "this is confirmation of your appointment at " + momentSlotDateTime.format('hh:mmA')+
+                "this is confirmation of your appointment at " + momentSlotDateTime.format('hh:mmA') +
                 ' on ' + momentSlotDateTime.format('dddd, DD of MMM') + '. Please be online at the specified time.';
 
             if (!err) {
-                if(data.mobile) {
+                if (data.mobile) {
                     clockwork.sendSms({To: data.mobile, Content: content},
                         function (error, resp) {
                             if (error) {
@@ -33,5 +34,37 @@
         });
     }
 
-    module.exports = {sendAppointmentSms: sendAppointmentSms};
+    function sendProviderAppointmentSms(providerId, slotDateTimeEpoch, callback) {
+        providersRepository.getOne(providerId, function (err, provider) {
+            var slotDateTime = new Date();
+            slotDateTime.setTime(slotDateTimeEpoch);
+            var momentSlotDateTime = moment(slotDateTime);
+            var content = "Dear " + provider.title + " " + provider.name + " " + provider.surname + ", " +
+                "this is confirmation that you have an appointment at " + momentSlotDateTime.format('hh:mmA') +
+                ' on ' + momentSlotDateTime.format('dddd, DD of MMM') + '. Please call the patient at the specified time.';
+
+            if (!err) {
+                var mobileNumber = provider.getMobileNumber();
+                if (mobileNumber) {
+                    clockwork.sendSms({To: mobileNumber.contact, Content: content},
+                        function (error, resp) {
+                            if (error) {
+                                console.log('Something went wrong', error);
+                                callback(error, null);
+                            } else {
+                                console.log('Message sent', resp);
+                                callback(null, 'Message sent', resp);
+                            }
+                        });
+                } else{
+                    callback(null, null);
+                }
+            }
+        });
+    }
+
+    module.exports = {
+        sendAppointmentSms: sendAppointmentSms,
+        sendProviderAppointmentSms: sendProviderAppointmentSms
+    };
 })();
