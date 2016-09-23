0033e1ccfdd21e80579e47d251048a2c54609f87
Victor Crudu <vcrudu@hotmail.com>
2016-09-23 23:33:56 +0100

    Some restyling

--- public/provider/ProviderRegisterCtrl.js
+++ public/provider/ProviderRegisterCtrl.js
@@ -1,89 +1,95 @@
 angular.module('app')
-    .controller('ProviderRegisterCtrl', ['common', 'dataaccess', '$window', 'ProviderService', 'Messaging',
-        '$filter','$modal', '$state', '$timeout',function (common, dataaccess, $window, ProviderService, Messaging,$filter,$modal, $state, $timeout) {
+    .controller('ProviderRegisterCtrl', ['$scope','common', 'dataaccess', '$window', 'ProviderService', 'Messaging',
+        '$filter','$modal', '$state', '$timeout',function ($scope, common, dataaccess, $window, ProviderService, Messaging,$filter,$modal, $state, $timeout) {
 
-        var vm = this;
+            var vm = this;
 
-        vm.isProviderMenuItemActive = true;
-        vm.isPatientMenuItemActive = false;
+            vm.isProviderMenuItemActive = true;
+            vm.isPatientMenuItemActive = false;
 
-        vm.goBack = function () {
-            $window.history.back();
-        };
+            vm.goBack = function () {
+                $state.go('login');
+            };
 
-        vm.titles = common.getPersonTitles();
-        vm.genders = common.getGenders();
-        vm.providerTypes = common.getProviderTypes();
-        common.getCountries()
-            .then(function (result) {
-                vm.countries = result.data;
-            });
-
-        vm.events = [];
-
-
-        vm.newProvider = {
-            type: "provider",
-            address: {},
-            contactDetails: [],
-            availabilityType: "regular",
-            availabilities: []
-        };
-
-        vm.isAvailabilityValid = function() {
-            if(vm.daysSchedule && vm.daysSchedule.length > 0){
-                angular.forEach(vm.daysSchedule, function(scheduleItem){
-                   if(scheduleItem.length>0 && scheduleItem!='NA'){
-                       return true;
-                   }
-                });
-            }
-            return false;
-        };
-
-        vm.save = function () {
-
-            var toSaveObject = angular.copy(vm.newProvider);
-
-            if (vm.mobile) {
-                toSaveObject.phone = vm.mobile;
-            }
-
-            angular.forEach(vm.daysSchedule, function (d) {
-
-                if (d.schedule && d.schedule.length) {
-
-                    var periods = d.schedule.split(',');
-
-                    angular.forEach(periods, function (period) {
-                        var day = {
-                            day: d.value,
-                            startTime: period.split('-')[0],
-                            endTime: period.split('-')[1]
-
-                        };
-                        toSaveObject.availabilities.push(day)
-                    })
-
+            vm.goForward = function () {
+                if(!$scope.firstForm.email.$pending){
+                    $scope.rc.sampleWizard.forward();
                 }
-            });
+            };
 
-            vm.saving = true;
-            ProviderService.save(toSaveObject)
+            vm.titles = common.getPersonTitles();
+            vm.genders = common.getGenders();
+            vm.providerTypes = common.getProviderTypes();
+            common.getCountries()
                 .then(function (result) {
-                vm.saving = false;
-                Messaging.success('sm', 'Success', 'Your details have been submitted successfully!');
-                $timeout(function() {
-                  $state.go('need-activate');
-                }, 1000);
+                    vm.countries = result.data;
+                });
 
-            }, function (e) {
-                vm.saving = false;
-                Messaging.errHandle(e);
+            vm.events = [];
 
-            });
-        }
-    }])
+
+            vm.newProvider = {
+                type: "provider",
+                address: {},
+                contactDetails: [],
+                availabilityType: "regular",
+                availabilities: []
+            };
+
+            vm.isAvailabilityValid = function () {
+                if (vm.daysSchedule && vm.daysSchedule.length > 0) {
+                    angular.forEach(vm.daysSchedule, function (scheduleItem) {
+                        if (scheduleItem.length > 0 && scheduleItem != 'NA') {
+                            return true;
+                        }
+                    });
+                }
+                return false;
+            };
+
+            vm.save = function () {
+
+                var toSaveObject = angular.copy(vm.newProvider);
+
+                if (vm.mobile) {
+                    toSaveObject.phone = vm.mobile;
+                }
+
+                angular.forEach(vm.daysSchedule, function (d) {
+
+                    if (d.schedule && d.schedule.length) {
+
+                        var periods = d.schedule.split(',');
+
+                        angular.forEach(periods, function (period) {
+                            var day = {
+                                day: d.value,
+                                startTime: period.split('-')[0],
+                                endTime: period.split('-')[1]
+
+                            };
+                            toSaveObject.availabilities.push(day)
+                        })
+
+                    }
+                });
+
+                vm.saving = true;
+                ProviderService.save(toSaveObject)
+                    .then(function (result) {
+                        vm.saving = false;
+                        Messaging.success('sm', 'Success', 'Your details have been submitted successfully!');
+                        $timeout(function () {
+                            $state.go('need-activate');
+                        }, 1000);
+
+                    }, function (e) {
+                        vm.saving = false;
+                        Messaging.errHandle(e);
+
+                    });
+            }
+        }])
     .directive('weekdaySchedule', ['common', function (common) {
         return {
             restrict: 'AE',
